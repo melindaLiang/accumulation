9a2ecfc8b4330f742a379e7cd1863fdb265d40aa
ymliang <ymliang@ibenben.com>
2018-12-14 18:50:23 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,11 @@ module.exports = {
   env: {
     browser: true,
   },
+  //除了支持插件外，eslint还支持通过扩展来快速的引入开源的JavaScript代码规则，减少了我们选择规则的时间，例如eslint官方推荐的规则(eslint:recommended),airbnb(airbnb)
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
